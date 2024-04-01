f4882329c32e82096cadf43b5450d79e053c3a6c
Valery_Khramov <dr.hramoff2013@yandex.ru>
2024-04-01 12:14:43 +0300

    added: lesson three

--- Lessons/lectures/lectures/urls.py
+++ Lessons/lectures/lectures/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from myapp3.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),
+    path('myapp', include('myapp.urls')),
+    path('myapp3/', include('myapp3.urls')),
+    path('', index),
 ]
